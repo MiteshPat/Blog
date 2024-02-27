da4aa7c42002a682274e131e0adc4f5e1719154b
Mitesh <mitesh10001.js@gmail.com>
2024-02-27 12:08:53 +0000

    Added a method for data entry of posts into blogs

--- blogs/urls.py
+++ blogs/urls.py
@@ -11,4 +11,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     # page that shows all blogs
     path('blogs/', views.blogs, name='blogs'),
+    # detail posts for a single blog
+    path('blogs/<int:blog_id>/', views.blog, name='blog'),
 ]
--- blogs/views.py
+++ blogs/views.py
@@ -12,3 +12,13 @@ def blogs(request):
     context = {'blogs': blogs}
     return render(request, 'blogs/blogs.xhtml', context)
 
+def blog(request, blog_id):
+    # show a single blog and all it's posts
+    blog = Blog.objects.get(id=blog_id)
+
+    posts = blog.post_set.order_by('-date_added')
+
+    context = {'blog': blog, 'posts': posts}
+
+    return render(request, 'blogs/blog.xhtml', context)
+
